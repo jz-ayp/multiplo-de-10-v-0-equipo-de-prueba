e7187844f4819e153b59f40dc43bbd77a87e0551
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-25 13:47:52 +0000

    Primer intento

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Escribe tu programa a continuación
+# Entradas
+numero = int(input("Introduce un número: "))
+
+# Proceso
+if numero % 10 == 0:
+    resultado = "sí"
+else:
+    resultado = "no"
+
+# Salidas
+print("El número", numero, resultado, "es divisible entre 10")
